adadfb119cb29bc4a8e02104517d3f7e0286cc1b
Britt <brittanylmcnabb1998@hotmail.com>
2023-01-31 18:53:33 -0500

    Add project files.

---
+++ TicTacToe.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace BMcNabbAssignment1
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGameTitle = new System.Windows.Forms.Label();
+            this.lblGameTitleEdition = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayersTurn = new System.Windows.Forms.Label();
+            this.pBoxC3 = new System.Windows.Forms.PictureBox();
+            this.pBoxC2 = new System.Windows.Forms.PictureBox();
+            this.pBoxC1 = new System.Windows.Forms.PictureBox();
+            this.pBoxB3 = new System.Windows.Forms.PictureBox();
+            this.pBoxB2 = new System.Windows.Forms.PictureBox();
+            this.pBoxB1 = new System.Windows.Forms.PictureBox();
+            this.pBoxA3 = new System.Windows.Forms.PictureBox();
+            this.pBoxA2 = new System.Windows.Forms.PictureBox();
+            this.pBoxA1 = new System.Windows.Forms.PictureBox();
+            this.pBoxPlayer1 = new System.Windows.Forms.PictureBox();
+            this.pBoxPlayer2 = new System.Windows.Forms.PictureBox();
+            this.pBoxScaryTree = new System.Windows.Forms.PictureBox();
+            this.pBoxScaryPumpkin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxA3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxA2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxScaryTree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxScaryPumpkin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGameTitle
+            // 
+            this.lblGameTitle.AutoSize = true;
+            this.lblGameTitle.Font = new System.Drawing.Font("MS Gothic", 26.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblGameTitle.Location = new System.Drawing.Point(332, 81);
+            this.lblGameTitle.Name = "lblGameTitle";
+            this.lblGameTitle.Size = new System.Drawing.Size(532, 87);
+            this.lblGameTitle.TabIndex = 0;
+            this.lblGameTitle.Text = "Tic-Tac-Toe";
+            // 
+            // lblGameTitleEdition
+            // 
+            this.lblGameTitleEdition.AutoSize = true;
+            this.lblGameTitleEdition.Font = new System.Drawing.Font("MS PGothic", 14.1F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameTitleEdition.ForeColor = System.Drawing.Color.Black;
+            this.lblGameTitleEdition.Location = new System.Drawing.Point(423, 205);
+            this.lblGameTitleEdition.Name = "lblGameTitleEdition";
+            this.lblGameTitleEdition.Size = new System.Drawing.Size(326, 47);
+            this.lblGameTitleEdition.TabIndex = 1;
+            this.lblGameTitleEdition.Text = "Spooks Edition";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.ForeColor = System.Drawing.Color.Black;
+            this.lblPlayer2.Location = new System.Drawing.Point(1153, 835);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(185, 40);
+            this.lblPlayer2.TabIndex = 2;
+            this.lblPlayer2.Text = "Player 2";
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.ForeColor = System.Drawing.Color.Black;
+            this.lblPlayer1.Location = new System.Drawing.Point(1153, 516);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(185, 40);
+            this.lblPlayer1.TabIndex = 3;
+            this.lblPlayer1.Text = "Player 1";
+            // 
+            // lblPlayersTurn
+            // 
+            this.lblPlayersTurn.AutoSize = true;
+            this.lblPlayersTurn.Font = new System.Drawing.Font("MS Gothic", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayersTurn.ForeColor = System.Drawing.Color.Black;
+            this.lblPlayersTurn.Location = new System.Drawing.Point(368, 1164);
+            this.lblPlayersTurn.Name = "lblPlayersTurn";
+            this.lblPlayersTurn.Size = new System.Drawing.Size(420, 47);
+            this.lblPlayersTurn.TabIndex = 4;
+            this.lblPlayersTurn.Text = "Player 1 Starts!";
+            // 
+            // pBoxC3
+            // 
+            this.pBoxC3.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxC3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxC3.Location = new System.Drawing.Point(717, 844);
+            this.pBoxC3.Name = "pBoxC3";
+            this.pBoxC3.Size = new System.Drawing.Size(279, 255);
+            this.pBoxC3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxC3.TabIndex = 13;
+            this.pBoxC3.TabStop = false;
+            this.pBoxC3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxC2
+            // 
+            this.pBoxC2.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxC2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxC2.Location = new System.Drawing.Point(432, 844);
+            this.pBoxC2.Name = "pBoxC2";
+            this.pBoxC2.Size = new System.Drawing.Size(279, 255);
+            this.pBoxC2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxC2.TabIndex = 12;
+            this.pBoxC2.TabStop = false;
+            this.pBoxC2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxC1
+            // 
+            this.pBoxC1.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxC1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxC1.Location = new System.Drawing.Point(147, 844);
+            this.pBoxC1.Name = "pBoxC1";
+            this.pBoxC1.Size = new System.Drawing.Size(279, 255);
+            this.pBoxC1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxC1.TabIndex = 11;
+            this.pBoxC1.TabStop = false;
+            this.pBoxC1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxB3
+            // 
+            this.pBoxB3.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxB3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxB3.Location = new System.Drawing.Point(717, 583);
+            this.pBoxB3.Name = "pBoxB3";
+            this.pBoxB3.Size = new System.Drawing.Size(279, 255);
+            this.pBoxB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxB3.TabIndex = 10;
+            this.pBoxB3.TabStop = false;
+            this.pBoxB3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxB2
+            // 
+            this.pBoxB2.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxB2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxB2.Location = new System.Drawing.Point(431, 583);
+            this.pBoxB2.Name = "pBoxB2";
+            this.pBoxB2.Size = new System.Drawing.Size(279, 255);
+            this.pBoxB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxB2.TabIndex = 9;
+            this.pBoxB2.TabStop = false;
+            this.pBoxB2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxB1
+            // 
+            this.pBoxB1.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxB1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxB1.Location = new System.Drawing.Point(147, 583);
+            this.pBoxB1.Name = "pBoxB1";
+            this.pBoxB1.Size = new System.Drawing.Size(279, 255);
+            this.pBoxB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxB1.TabIndex = 8;
+            this.pBoxB1.TabStop = false;
+            this.pBoxB1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxA3
+            // 
+            this.pBoxA3.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxA3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxA3.Location = new System.Drawing.Point(717, 322);
+            this.pBoxA3.Name = "pBoxA3";
+            this.pBoxA3.Size = new System.Drawing.Size(279, 255);
+            this.pBoxA3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxA3.TabIndex = 7;
+            this.pBoxA3.TabStop = false;
+            this.pBoxA3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxA2
+            // 
+            this.pBoxA2.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxA2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxA2.Location = new System.Drawing.Point(431, 322);
+            this.pBoxA2.Name = "pBoxA2";
+            this.pBoxA2.Size = new System.Drawing.Size(280, 255);
+            this.pBoxA2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxA2.TabIndex = 6;
+            this.pBoxA2.TabStop = false;
+            this.pBoxA2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxA1
+            // 
+            this.pBoxA1.BackColor = System.Drawing.Color.Transparent;
+            this.pBoxA1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxA1.Location = new System.Drawing.Point(147, 322);
+            this.pBoxA1.Name = "pBoxA1";
+            this.pBoxA1.Size = new System.Drawing.Size(279, 255);
+            this.pBoxA1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxA1.TabIndex = 5;
+            this.pBoxA1.TabStop = false;
+            this.pBoxA1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pBoxPlayer1
+            // 
+            this.pBoxPlayer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxPlayer1.Image = global::BMcNabbAssignment1.Properties.Resources.x;
+            this.pBoxPlayer1.Location = new System.Drawing.Point(1160, 322);
+            this.pBoxPlayer1.Name = "pBoxPlayer1";
+            this.pBoxPlayer1.Size = new System.Drawing.Size(178, 168);
+            this.pBoxPlayer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxPlayer1.TabIndex = 14;
+            this.pBoxPlayer1.TabStop = false;
+            // 
+            // pBoxPlayer2
+            // 
+            this.pBoxPlayer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxPlayer2.Image = global::BMcNabbAssignment1.Properties.Resources.o;
+            this.pBoxPlayer2.Location = new System.Drawing.Point(1160, 647);
+            this.pBoxPlayer2.Name = "pBoxPlayer2";
+            this.pBoxPlayer2.Size = new System.Drawing.Size(178, 168);
+            this.pBoxPlayer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxPlayer2.TabIndex = 15;
+            this.pBoxPlayer2.TabStop = false;
+            // 
+            // pBoxScaryTree
+            // 
+            this.pBoxScaryTree.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxScaryTree.Image = global::BMcNabbAssignment1.Properties.Resources.scary_tree;
+            this.pBoxScaryTree.Location = new System.Drawing.Point(1258, 955);
+            this.pBoxScaryTree.Name = "pBoxScaryTree";
+            this.pBoxScaryTree.Size = new System.Drawing.Size(221, 295);
+            this.pBoxScaryTree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxScaryTree.TabIndex = 16;
+            this.pBoxScaryTree.TabStop = false;
+            // 
+            // pBoxScaryPumpkin
+            // 
+            this.pBoxScaryPumpkin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pBoxScaryPumpkin.Image = global::BMcNabbAssignment1.Properties.Resources.scary_pumpkin_3;
+            this.pBoxScaryPumpkin.Location = new System.Drawing.Point(60, 40);
+            this.pBoxScaryPumpkin.Name = "pBoxScaryPumpkin";
+            this.pBoxScaryPumpkin.Size = new System.Drawing.Size(171, 235);
+            this.pBoxScaryPumpkin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxScaryPumpkin.TabIndex = 17;
+            this.pBoxScaryPumpkin.TabStop = false;
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(1491, 1262);
+            this.Controls.Add(this.pBoxScaryPumpkin);
+            this.Controls.Add(this.pBoxScaryTree);
+            this.Controls.Add(this.pBoxPlayer2);
+            this.Controls.Add(this.pBoxPlayer1);
+            this.Controls.Add(this.pBoxC3);
+            this.Controls.Add(this.pBoxC2);
+            this.Controls.Add(this.pBoxC1);
+            this.Controls.Add(this.pBoxB3);
+            this.Controls.Add(this.pBoxB2);
+            this.Controls.Add(this.pBoxB1);
+            this.Controls.Add(this.pBoxA3);
+            this.Controls.Add(this.pBoxA2);
+            this.Controls.Add(this.pBoxA1);
+            this.Controls.Add(this.lblPlayersTurn);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblGameTitleEdition);
+            this.Controls.Add(this.lblGameTitle);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Name = "TicTacToe";
+            this.Text = "Tic-Tac-Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxA3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxA2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxScaryTree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxScaryPumpkin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGameTitle;
+        private System.Windows.Forms.Label lblGameTitleEdition;
+        private System.Windows.Forms.Label lblPlayer2;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayersTurn;
+        private System.Windows.Forms.PictureBox pBoxA1;
+        private System.Windows.Forms.PictureBox pBoxA2;
+        private System.Windows.Forms.PictureBox pBoxA3;
+        private System.Windows.Forms.PictureBox pBoxB1;
+        private System.Windows.Forms.PictureBox pBoxB2;
+        private System.Windows.Forms.PictureBox pBoxB3;
+        private System.Windows.Forms.PictureBox pBoxC1;
+        private System.Windows.Forms.PictureBox pBoxC2;
+        private System.Windows.Forms.PictureBox pBoxC3;
+        private System.Windows.Forms.PictureBox pBoxPlayer1;
+        private System.Windows.Forms.PictureBox pBoxPlayer2;
+        private System.Windows.Forms.PictureBox pBoxScaryTree;
+        private System.Windows.Forms.PictureBox pBoxScaryPumpkin;
+    }
+}
+
---
+++ TicTacToe.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMcNabbAssignment1
+{
+    /// <summary>
+    /// This application runs a classic Tic-Tac-Toe game; With a spooky twist. 
+    /// Brittany McNabb (7853724).
+    /// Sunday October 2nd, 2022. 
+    /// </summary>
+
+    //Image references: 
+    //Chaos07. (2018-2022). Pumpkin clipart[PNG]. Creazilla. https://creazilla.com/nodes/3162124-pumpkin-clipart
+    //Creazilla. (2018-2022).Haunted tree clipart[PNG]. Creazilla. https://creazilla.com/nodes/19020-haunted-tree-clipart
+    //LATIN CAPITAL LETTER O [PNG]. (2022). Wiktionary. https://en.wiktionary.org/wiki/O
+    //Bequw. (2009). Letter x [SVG]. Wikimedia. https://commons.wikimedia.org/wiki/File:Letter_x.svg
+
+    public partial class TicTacToe : Form
+    {
+
+        //Image resources for the game.
+        Image o = BMcNabbAssignment1.Properties.Resources.o;
+        Image x = BMcNabbAssignment1.Properties.Resources.x;
+        Image scary_Pumpkin = BMcNabbAssignment1.Properties.Resources.scary_pumpkin_3;
+        Image scary_Tree = BMcNabbAssignment1.Properties.Resources.scary_tree;
+
+        //Declared variables. 
+        string the_Winner = "";
+        bool players_Turn = true;
+        bool did_Someone_Win = false;
+        int grid_count = 0;
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+            //The Tic-Tac-Toe form has picture boxes which can be retrieved from the parameter "sender". 
+            PictureBox grid = sender as PictureBox;
+
+            //If the user clicks on the picture box and the bool (players_Turn) equals true:          
+            if (players_Turn)
+            {
+                //X starts - The image "X" will appear in the picture box. 
+                grid.Image = x;
+
+                //The label at the bottom of the board will change to "Player 2's turn". 
+                lblPlayersTurn.Text = "Player 2's turn";
+            }
+            else
+            {
+                //The same logic applies here. 
+                grid.Image = o;
+                lblPlayersTurn.Text = "Player 1's turn";
+            }
+
+            //In order to switch the players turn: 
+            //If the bool (players_Turn) is true, set true to not true. 
+            //If the bool (players_Turn) is false, set false to not false. 
+            players_Turn = !players_Turn;
+
+            //The picture box that was clicked will be disabled. 
+            grid.Enabled = false;
+
+            //1 will be added to the grid count. 
+            grid_count++;
+
+            //This method is called to see if there's a winner. 
+            checkForWinner();
+        }
+
+        public void checkForWinner()
+        {
+            //Check horizontal rows
+            if ((pBoxA1.Image == pBoxA2.Image) && (pBoxA2.Image == pBoxA3.Image) && (pBoxA1.Image != null)) { did_Someone_Win = true; }
+            else if ((pBoxB1.Image == pBoxB2.Image) && (pBoxB2.Image == pBoxB3.Image) && (pBoxB1.Image != null)) { did_Someone_Win = true; }
+            else if ((pBoxC1.Image == pBoxC2.Image) && (pBoxC2.Image == pBoxC3.Image) && (pBoxC1.Image != null)) { did_Someone_Win = true; }
+
+            //Check vertical rows
+            if ((pBoxA1.Image == pBoxB1.Image) && (pBoxB1.Image == pBoxC1.Image) && (pBoxA1.Image != null)) { did_Someone_Win = true; }
+            else if ((pBoxA2.Image == pBoxB2.Image) && (pBoxB2.Image == pBoxC2.Image) && (pBoxA2.Image != null)) { did_Someone_Win = true; }
+            else if ((pBoxA3.Image == pBoxB3.Image) && (pBoxB3.Image == pBoxC3.Image) && (pBoxA3.Image != null)) { did_Someone_Win = true; }
+
+            //Check diagonal rows
+            if ((pBoxA3.Image == pBoxB2.Image) && (pBoxB2.Image == pBoxC1.Image) && (pBoxA3.Image != null)) { did_Someone_Win = true; }
+            if ((pBoxA1.Image == pBoxB2.Image) && (pBoxB2.Image == pBoxC3.Image) && (pBoxA1.Image != null)) { did_Someone_Win = true; }
+
+            //If did_Someone_Win is equal to true:
+            if (did_Someone_Win)
+            {
+                //If the bool (players_Turn) is equal to true:
+                if (players_Turn)
+                {
+                    //Player 2 wins. 
+                    the_Winner = "Player 2 - O";
+                }
+                else
+                {
+                    //Otherwise, Player 1 wins. 
+                    the_Winner = "Player 1 - X";
+                }
+
+                //A message box appears declaring the winner. 
+                var userInteraction = MessageBox.Show(the_Winner + " wins!\nPress OK to play again.", "Boo!", MessageBoxButtons.OK);
+
+                //If the user clicks OK:
+                if (userInteraction == DialogResult.OK)
+                {
+                    //This method is called to start a new game. 
+                    NewGame();
+                }           
+            }
+            else
+            {
+                //If the grid count reaches 9:
+                if (grid_count == 9)
+                {
+                    //All the picture boxes have been selected and the game ends in a tie. 
+                    MessageBox.Show("It's a tie!");
+                    //This method is called to start a new game. 
+                    NewGame();
+                }
+            }
+        }
+
+        public void NewGame()
+        {
+            //The bool (players_Turn) is set back to true:
+            //X must start first. 
+            players_Turn = true;
+
+            //The grid count is set to 0 - keep track of a possible tie for the next game. 
+            grid_count = 0;
+
+            //The label shows which player will start the new game. 
+            lblPlayersTurn.Text = "Player 1 Starts!";
+
+            //This foreach statement loops throught the controls, in this case picture boxes, of the form: 
+            foreach (var pictureBox in Controls.OfType<PictureBox>())
+            {
+                //Each picture box image is set to null - Erases the image. 
+                pictureBox.Image = null;
+                //All the picture boxes are enabled again - They can be clicked by the user to show an X or O. 
+                pictureBox.Enabled = true;
+            }
+
+            //The scary pumpkin, tree, X, and O images will re-appear after all the picture boxes have been set to null.
+            pBoxScaryPumpkin.Image = scary_Pumpkin;
+            pBoxScaryTree.Image = scary_Tree;
+            pBoxPlayer1.Image = x;
+            pBoxPlayer2.Image = o;
+
+            //The bool (did_Someone_Win) is set to false to check for a winner again. 
+            did_Someone_Win = false; 
+        }
+    }
+}
